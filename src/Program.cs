b9902c8fdf58d95591c1484d1d611ac5b99f9ac5
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-05-03 19:20:40 -0300

    some shit

--- src/Program.cs
+++ src/Program.cs
@@ -1,55 +1,37 @@
-﻿public class Program
+﻿using static System.Console;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        // Console.Write("Informe o valor total de elementos: ");
-        // double n = double.Parse(Console.ReadLine());
+        Write("Infome a % de sucesso em decimal: ");
+        double s = double.Parse(ReadLine());
 
-        // Console.Write("Informe o número de combinações possíveis: ");
-        // double r = double.Parse(Console.ReadLine());
+        Write("Infome a % de falha em decimal: ");
+        double f = double.Parse(ReadLine());
 
-        // Func<double, double> fat = null;
-        // fat = x => x <= 1 ? 1 : x * fat(x - 1);
+        Write("Infome a quantidade da amostra: ");
+        double n = double.Parse(ReadLine());
 
-        // var c = fat(n) / (fat(r) * fat(n - r));
-
-        // Console.WriteLine($"C({n},{r}) = {c}");
-
-
-
-        // var total = c * Math.Pow(0.10, 2) * Math.Pow(0.90, 13);
-
-        // Console.WriteLine($"C(15,2) = {c} \nTotal = {total}");
-
-        Console.Write("Infome a % de sucesso em decimal: ");
-        double s = double.Parse(Console.ReadLine());
-
-        Console.Write("Infome a % de falha em decimal: ");
-        double f = double.Parse(Console.ReadLine());
-
-        Console.Write("Infome a quantidade da amostra: ");
-        double n = double.Parse(Console.ReadLine());
-
-        int resp = 1;
+        int retry = 1;
         do
         {
-            Console.Write("Informe o número de possibilidades (x=?): ");
-            double r = double.Parse(Console.ReadLine());
+            Write("Informe o número de possibilidades (x=?): ");
+            double r = double.Parse(ReadLine());
 
             Func<double, double> fat = null;
             fat = x => x <= 1 ? 1 : x * fat(x - 1);
 
             double c = fat(n) / (fat(r) * fat(n - r));
-
-            Console.WriteLine($"C({n},{r}) = {c}");
+            WriteLine($"C({n},{r}) = {c}");
 
             double ProbTotalDecimal = c * Math.Pow(s, r) * Math.Pow(f, (n-r));
             double ProbTotalPorcentagem = ProbTotalDecimal * 100;
-            Console.WriteLine($"P(x={r}) : {ProbTotalDecimal.ToString("n4")} -> {ProbTotalPorcentagem:n2}%");
+            WriteLine($"P(x={r}) : {ProbTotalDecimal.ToString("n4")} -> {ProbTotalPorcentagem:n2}%");
 
 
-            Console.Write("Digite {1} para realizar outra operação: ");
-            resp = int.Parse(Console.ReadLine());
-        } while (resp == 1);
+            Write("Digite {1} para realizar outra operação: ");
+            retry = int.Parse(ReadLine());
+        } while (retry == 1);
     }
 }
